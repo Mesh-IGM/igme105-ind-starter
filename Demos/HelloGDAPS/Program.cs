82073ac615bdb8a4da22aab78dd62b07fe7eeef2
Erika Mesh <esmvcs@rit.edu>
2026-08-26 11:30:49 -0400

    Update the demo to VS Community 2026 - .NET 10

--- Demos/HelloGDAPS/Program.cs
+++ Demos/HelloGDAPS/Program.cs
@@ -1,17 +1,17 @@
 ﻿/*
- * Demo project created by Prof. Mesh so that
- * everyone would have something in their new
- * GitHub repo from the start. :)
- */
+* Demo project created by Prof. Mesh so that
+* everyone would have something in their new
+* GitHub repo from the start. :)
+*/
 
-namespace HelloGit
+namespace HelloGDAPS
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             // Print some text to the console to be sure everything works
-            Console.WriteLine("Hello GDAPS1 student!");
+            Console.WriteLine("Hello GDAPS student!");
             Console.WriteLine("You successfully cloned and ran a project from GitHub!");
         }
     }
